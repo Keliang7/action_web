44884f2805f146bda8f915377f11ad31324b0c64
liangqi <1739484858@qq.com>
2025-10-24 16:32:59 +0800

    fix: Dockerfile 'as'->'AS',vite打包采用相对路径

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # 使用官方的 Node.js 镜像作为基础镜像
-FROM node:18.16.0-alpine as build-stage
+FROM node:18.16.0-alpine AS build-stage
 
 # 设置工作目录
 WORKDIR /app
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [vue()],
   server: {
     port: 4000,
